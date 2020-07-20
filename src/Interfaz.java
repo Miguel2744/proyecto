9d9ea99ff4958b50aff6baaea0827d39f7f27fff
DESKTOP-EG33FMS\ASUS <L18090234@zacatepec.tecnm.mx>
2020-07-20 14:10:38 -0500

    listo amigos

--- src/Interfaz.java
+++ src/Interfaz.java
@@ -1,3 +1,7 @@
+
+import javax.swing.JOptionPane;
+import javax.swing.table.DefaultTableModel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -9,13 +13,22 @@
  * @author ASUS
  */
 public class Interfaz extends javax.swing.JFrame {
+    
+   DefaultTableModel model=new DefaultTableModel();
+    
 
     /**
      * Creates new form Interfaz
      */
     public Interfaz() {
+        
         initComponents();
         this.setLocationRelativeTo(null);
+        model.addColumn("Id Zona");
+        model.addColumn("Longitud");
+        model.addColumn("Nombre Zona");
+        model.addColumn("Latitud");
+        this.tabla.setModel(model);
 
     }
 
@@ -28,22 +41,31 @@ public class Interfaz extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
+        jScrollPane2 = new javax.swing.JScrollPane();
+        jTable2 = new javax.swing.JTable();
         jCPanel1 = new com.bolivia.panel.JCPanel();
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
-        jLabel2 = new javax.swing.JLabel();
+        IDZONA = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
-        jLabel4 = new javax.swing.JLabel();
-        jLabel5 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
-        jTextField1 = new javax.swing.JTextField();
-        jTextField2 = new javax.swing.JTextField();
-        jTextField3 = new javax.swing.JTextField();
-        jTextField4 = new javax.swing.JTextField();
-        jTextField5 = new javax.swing.JTextField();
+        zonanombre = new javax.swing.JLabel();
+        Latitud = new javax.swing.JLabel();
+        txtidZona = new javax.swing.JTextField();
+        txtlongitud = new javax.swing.JTextField();
+        txtnombrezona = new javax.swing.JTextField();
+        txtlatitud = new javax.swing.JTextField();
+        btnguardar = new javax.swing.JButton();
+        btmlimpiar = new javax.swing.JButton();
+        jPanel4 = new javax.swing.JPanel();
+        txtbuscar = new javax.swing.JTextField();
+        btnbuscar = new javax.swing.JButton();
         jButton1 = new javax.swing.JButton();
-        jButton2 = new javax.swing.JButton();
+        jScrollPane3 = new javax.swing.JScrollPane();
+        tabla = new javax.swing.JTable();
+        btmAgregar = new javax.swing.JButton();
         jPanel3 = new javax.swing.JPanel();
         jLabel7 = new javax.swing.JLabel();
         jLabel8 = new javax.swing.JLabel();
@@ -57,6 +79,33 @@ public class Interfaz extends javax.swing.JFrame {
         jTextField10 = new javax.swing.JTextField();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
+        jButton5 = new javax.swing.JButton();
+
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane1.setViewportView(jTable1);
+
+        jTable2.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane2.setViewportView(jTable2);
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -93,33 +142,104 @@ public class Interfaz extends javax.swing.JFrame {
         jPanel2.setForeground(new java.awt.Color(0, 0, 153));
         jPanel2.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel2.setText("Id Zona:");
-        jPanel2.add(jLabel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(72, 41, -1, -1));
+        IDZONA.setText("Id Zona:");
+        jPanel2.add(IDZONA, new org.netbeans.lib.awtextra.AbsoluteConstraints(72, 41, -1, -1));
 
         jLabel3.setText("Longitud:");
         jPanel2.add(jLabel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(68, 79, -1, -1));
 
-        jLabel4.setText("Nombre de La Zona:");
-        jPanel2.add(jLabel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(16, 117, -1, -1));
+        zonanombre.setText("Nombre de La Zona:");
+        jPanel2.add(zonanombre, new org.netbeans.lib.awtextra.AbsoluteConstraints(16, 117, -1, -1));
 
-        jLabel5.setText("Latitud:");
-        jPanel2.add(jLabel5, new org.netbeans.lib.awtextra.AbsoluteConstraints(62, 155, -1, -1));
+        Latitud.setText("Latitud:");
+        jPanel2.add(Latitud, new org.netbeans.lib.awtextra.AbsoluteConstraints(62, 155, -1, -1));
+        jPanel2.add(txtidZona, new org.netbeans.lib.awtextra.AbsoluteConstraints(134, 38, 111, -1));
+        jPanel2.add(txtlongitud, new org.netbeans.lib.awtextra.AbsoluteConstraints(134, 76, 111, -1));
+        jPanel2.add(txtnombrezona, new org.netbeans.lib.awtextra.AbsoluteConstraints(134, 114, 111, -1));
+        jPanel2.add(txtlatitud, new org.netbeans.lib.awtextra.AbsoluteConstraints(134, 152, 111, -1));
 
-        jLabel6.setText("Nombre de la Planta:");
-        jPanel2.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(16, 196, -1, -1));
-        jPanel2.add(jTextField1, new org.netbeans.lib.awtextra.AbsoluteConstraints(134, 38, 111, -1));
-        jPanel2.add(jTextField2, new org.netbeans.lib.awtextra.AbsoluteConstraints(134, 76, 111, -1));
-        jPanel2.add(jTextField3, new org.netbeans.lib.awtextra.AbsoluteConstraints(134, 114, 111, -1));
-        jPanel2.add(jTextField4, new org.netbeans.lib.awtextra.AbsoluteConstraints(134, 152, 111, -1));
-        jPanel2.add(jTextField5, new org.netbeans.lib.awtextra.AbsoluteConstraints(134, 190, 111, -1));
+        btnguardar.setText("Eliminar");
+        btnguardar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnguardarActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btnguardar, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 150, -1, -1));
 
-        jButton1.setText("GUARDAR");
-        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(227, 250, -1, -1));
+        btmlimpiar.setText("LIMPIAR");
+        btmlimpiar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btmlimpiarActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btmlimpiar, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 100, -1, -1));
 
-        jButton2.setText("LIMPIAR");
-        jPanel2.add(jButton2, new org.netbeans.lib.awtextra.AbsoluteConstraints(348, 250, -1, -1));
+        jPanel4.setBackground(new java.awt.Color(153, 185, 27));
 
-        jCPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 152, -1, -1));
+        txtbuscar.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
+
+        btnbuscar.setIcon(new javax.swing.ImageIcon(getClass().getResource("/imagenes/icons8_Search_32px_2.png"))); // NOI18N
+        btnbuscar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnbuscarActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel4Layout = new javax.swing.GroupLayout(jPanel4);
+        jPanel4.setLayout(jPanel4Layout);
+        jPanel4Layout.setHorizontalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addGap(20, 20, 20)
+                .addComponent(txtbuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 150, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(btnbuscar, javax.swing.GroupLayout.PREFERRED_SIZE, 54, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        jPanel4Layout.setVerticalGroup(
+            jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel4Layout.createSequentialGroup()
+                .addContainerGap()
+                .addGroup(jPanel4Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtbuscar)
+                    .addComponent(btnbuscar, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
+        );
+
+        jPanel2.add(jPanel4, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 190, -1, 60));
+
+        jButton1.setText("salir");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+        jPanel2.add(jButton1, new org.netbeans.lib.awtextra.AbsoluteConstraints(330, 230, -1, -1));
+
+        tabla.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jScrollPane3.setViewportView(tabla);
+
+        jPanel2.add(jScrollPane3, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 270, -1, 90));
+
+        btmAgregar.setText("AGREGAR");
+        btmAgregar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btmAgregarActionPerformed(evt);
+            }
+        });
+        jPanel2.add(btmAgregar, new org.netbeans.lib.awtextra.AbsoluteConstraints(320, 40, -1, -1));
+
+        jCPanel1.add(jPanel2, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 152, 460, 420));
 
         jPanel3.setBackground(new java.awt.Color(255, 255, 255));
         jPanel3.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Datos De Cultivos", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Curlz MT", 2, 18), new java.awt.Color(0, 0, 204))); // NOI18N
@@ -151,6 +271,9 @@ public class Interfaz extends javax.swing.JFrame {
         jButton4.setText("LIMPIAR");
         jPanel3.add(jButton4, new org.netbeans.lib.awtextra.AbsoluteConstraints(357, 250, -1, -1));
 
+        jButton5.setText("Grafica");
+        jPanel3.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(130, 230, -1, 20));
+
         jCPanel1.add(jPanel3, new org.netbeans.lib.awtextra.AbsoluteConstraints(579, 152, -1, 280));
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -167,6 +290,85 @@ public class Interfaz extends javax.swing.JFrame {
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
+    private void btnguardarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnguardarActionPerformed
+       int eli=tabla.getSelectedRowCount();
+       if(eli >=0)
+       {
+           model.removeRow(eli);
+       }else{
+           JOptionPane.showInternalMessageDialog(null, "No HAY DATOS POR ELIMINAR ");
+           
+       }
+        
+        
+    }//GEN-LAST:event_btnguardarActionPerformed
+
+    private void btnbuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnbuscarActionPerformed
+        // TODO add your handling code here:
+        String buscar=txtbuscar.getText();
+        botones.buscar(buscar);
+        
+        //limpia
+        txtbuscar.setText("");
+    }//GEN-LAST:event_btnbuscarActionPerformed
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+        System.exit(0);
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void btmAgregarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btmAgregarActionPerformed
+        // TODO add your handling code here:
+        
+           
+        
+        ///////
+        String [] agregar=new String[4];
+        agregar[0]=txtidZona.getText();
+        agregar[1]=txtlongitud.getText();
+        agregar[2]=txtnombrezona.getText();
+        agregar[3]=txtlatitud.getText();
+        model.addRow(agregar);
+        
+        
+         String id=txtidZona.getText();
+        String lon=txtlongitud.getText();
+        String znom=txtnombrezona.getText();
+        String lat=txtlatitud.getText();
+        
+      
+        //int  var=Integer.parseInt(d);}
+        
+        botones.guardar(id, lat, znom, lat);
+        //botones es el nombre de la clase principal 
+        //metodo creado 
+        //limpiar datos dentro del boton guardar 
+        txtidZona.setText("");
+        txtlongitud.setText("");
+        txtnombrezona.setText("");
+        txtlatitud.setText("");
+    }//GEN-LAST:event_btmAgregarActionPerformed
+
+    private void btmlimpiarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btmlimpiarActionPerformed
+        // TODO add your handling code here:
+        txtidZona.setText("");
+        txtlongitud.setText("");
+        txtnombrezona.setText("");
+        txtlatitud.setText("");
+    }//GEN-LAST:event_btmlimpiarActionPerformed
+
+     
+    public void cargardatos(Interfazz inter){
+        //LLENA LOS CAMPOS AL MOMENTO DE BUSCAR 
+        
+        this.txtidZona.setText(inter.getIDZONA());
+        this.txtlongitud.setText(inter.getLongitud());
+        this.txtnombrezona.setText(inter.getZonanombre());
+        this.txtlatitud.setText(inter.getLatitud());
+        
+        
+        
+    }
     /**
      * @param args the command line arguments
      */
@@ -203,34 +405,44 @@ public class Interfaz extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JLabel IDZONA;
+    private javax.swing.JLabel Latitud;
+    private javax.swing.JButton btmAgregar;
+    private javax.swing.JButton btmlimpiar;
+    private javax.swing.JButton btnbuscar;
+    private javax.swing.JButton btnguardar;
     private javax.swing.JButton jButton1;
-    private javax.swing.JButton jButton2;
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
+    private javax.swing.JButton jButton5;
     private com.bolivia.panel.JCPanel jCPanel1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel11;
-    private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
-    private javax.swing.JLabel jLabel4;
-    private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
     private javax.swing.JLabel jLabel8;
     private javax.swing.JLabel jLabel9;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JPanel jPanel3;
-    private javax.swing.JTextField jTextField1;
+    private javax.swing.JPanel jPanel4;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane3;
+    private javax.swing.JTable jTable1;
+    private javax.swing.JTable jTable2;
     private javax.swing.JTextField jTextField10;
-    private javax.swing.JTextField jTextField2;
-    private javax.swing.JTextField jTextField3;
-    private javax.swing.JTextField jTextField4;
-    private javax.swing.JTextField jTextField5;
     private javax.swing.JTextField jTextField6;
     private javax.swing.JTextField jTextField7;
     private javax.swing.JTextField jTextField8;
     private javax.swing.JTextField jTextField9;
+    private javax.swing.JTable tabla;
+    private javax.swing.JTextField txtbuscar;
+    private javax.swing.JTextField txtidZona;
+    private javax.swing.JTextField txtlatitud;
+    private javax.swing.JTextField txtlongitud;
+    private javax.swing.JTextField txtnombrezona;
+    private javax.swing.JLabel zonanombre;
     // End of variables declaration//GEN-END:variables
 }
